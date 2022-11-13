9ea0b2f89e423246ac5575fb46d6406bb10029dd
Watson Chua <watsonchua@gmail.com>
2022-11-13 19:53:00 +0800

    make multi pages with search and compare

---
+++ Main.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+from time import perf_counter
+import toml
+import matplotlib.pyplot as plt
+import math
+import plotly.express as px
+
+
+
+# secrets = toml.load('.streamlit/secrets.toml')
+# es_username = secrets['es_username']
+# es_password = secrets['es_password']
+
+
+# @st.cache(ttl=1*60*60)
+def read_data():
+    if 'df_profile' not in st.session_state:
+        st.session_state['df_profile'] = pd.read_csv('./profile.csv').sort_values('NAME', ascending=True)
+    if 'df_names' not in st.session_state:
+        st.session_state['df_names'] = pd.read_csv('./aipf_names.csv').sort_values('Name', ascending=True)
+    # return df.sort_values('NAME', ascending=True)
+
+# def make_grid(num_rows,num_cols):
+#     grid = [0]*num_rows
+#     for i in range(num_rows):
+#         with st.container():
+#             grid[i] = st.columns(num_cols)
+#     return grid
+
+def main():
+    read_data()
+
+    df_profiles = st.session_state['df_profile']
+
+    st.set_page_config(
+        layout="wide",
+        page_title="AIPF Emergenetics Profiles"
+        )
+    st.title("AIPF Emergenetics Profiles")
+
+    labels = ['ANALYTICAL', 'CONCEPTUAL','STRUCTURAL', 'SOCIAL']
+
+    with st.container():
+        mean_values = df_profiles[labels].mean().to_numpy()
+
+        fig, ax = plt.subplots()
+        ax.pie(mean_values, labels=labels, autopct='%1.2f%%', startangle=90)
+        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        
+        st.write('AIPF Overall')
+        st.pyplot(fig)
+        plt.close(fig)
+
+    for l in labels:
+        with st.expander('Top 5: ' + l.title()):
+            df_top = df_profiles.nlargest(5, l).sort_values(l, ascending=True)
+            fig=px.bar(df_top, y='NAME', x=l, orientation='h',color=l)
+            st.write(fig)
+
+
+    
+   
+
+
+if __name__ == "__main__":
+    main()
--- pages/Search_and _Compare.py
+++ pages/Search_and _Compare.py
@@ -5,6 +5,8 @@ from time import perf_counter
 import toml
 import matplotlib.pyplot as plt
 import math
+from Main import read_data
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 # secrets = toml.load('.streamlit/secrets.toml')
@@ -12,10 +14,13 @@ import math
 # es_password = secrets['es_password']
 
 
-@st.cache(ttl=1*60*60)
-def read_data():
-    df = pd.read_csv('./profile.csv')
-    return df.sort_values('NAME', ascending=True)
+# @st.cache(ttl=1*60*60)
+# def read_data():
+#     if 'df_profile' not in st.session_state:
+#         st.session_state['df_profile'] = pd.read_csv('./profile.csv').sort_values('NAME', ascending=True)
+#     if 'df_names' not in st.session_state:
+#         st.session_state['df_names'] = pd.read_csv('./aipf_names.csv').sort_values('Name', ascending=True)
+#     # return df.sort_values('NAME', ascending=True)
 
 def make_grid(num_rows,num_cols):
     grid = [0]*num_rows
@@ -25,6 +30,10 @@ def make_grid(num_rows,num_cols):
     return grid
 
 def main():
+    read_data()
+
+    df_profiles = st.session_state['df_profile']
+
     st.set_page_config(
         layout="wide",
         page_title="AIPF Emergenetics Profiles"
@@ -33,9 +42,6 @@ def main():
 
     labels = ['ANALYTICAL', 'CONCEPTUAL','STRUCTURAL', 'SOCIAL']
     
-    df_profiles = read_data()
-    num_cols = 3
-    num_rows = math.ceil(len(df_profiles)/num_cols)
 
     st.sidebar.title("Filters")
 
@@ -49,6 +55,13 @@ def main():
 
     with st.container():
         df_filtered = df_profiles[df_profiles['NAME'].isin(names_filter)]
+        if len(df_filtered) < 3:
+            num_rows = 1
+            num_cols = max(1, len(df_filtered))
+        else:
+            num_cols = 3
+            num_rows = math.ceil(len(df_filtered)/num_cols)
+
         grid = make_grid(num_rows, num_cols)
         row_no = 0
         col_no = 0
@@ -67,6 +80,14 @@ def main():
             counter += 1
             row_no = counter // num_cols
             col_no = counter % num_cols
+        
+        if len(df_filtered) == 2:
+            compare_vectors = df_filtered[labels].to_numpy()
+            similarity_score = cosine_similarity(compare_vectors[0].reshape(1,-1), compare_vectors[1].reshape(1,-1))
+            with st.container():
+                st.write('Similarity: ' + "{:.3f}".format(similarity_score[0][0]))
+
+        
 
 
 if __name__ == "__main__":
